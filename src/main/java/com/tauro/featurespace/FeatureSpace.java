d1c3c69c99e342b3ad9bc4a8bd93c314c4139b17
Amit Tauro <tauroamit@gmail.com>
2021-03-17 19:59:56 +0000

    first passing test

--- src/main/java/com/tauro/featurespace/FeatureSpace.java
+++ src/main/java/com/tauro/featurespace/FeatureSpace.java
@@ -1,18 +1,44 @@
 package com.tauro.featurespace;
 
+//import org.json.simple.JSONObject;
+import java.io.File;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import org.json.JSONObject;
+import org.json.JSONString;
+
+
 
 public class FeatureSpace {
 
-    private JSONObject[] events;
+    private String file;
 
-    public FeatureSpace(JSONObject[] eventList) {
-        events = eventList;
+    public FeatureSpace(String uploadedFile) {
+
+        file = uploadedFile;
     }
 
-    public String customersHighestAverageTransactionAmount() {
-        JSONObject customer = events[0];
-        String customerName = customer.getString("customerId");
-        return customerName;
+    public void customersHighestAverageTransactionAmount() {
+//        JSONObject customer = events[0];
+//        Object customerName = customer.get("customerId");
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader(
+                    file));
+            String line = reader.readLine();
+            JSONObject obj = new JSONObject(line);
+            String pageName = obj.getString("customerId");
+            System.out.print(pageName);
+            while (line != null) {
+//                System.out.println(line);
+                // read next line
+                line = reader.readLine();
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/test/java/com/tauro/featurespace/FeatureSpaceTest.java
+++ src/test/java/com/tauro/featurespace/FeatureSpaceTest.java
@@ -1,11 +1,25 @@
 package com.tauro.featurespace;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.json.JSONObject;
+import org.json.simple.JSONObject;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FeatureSpaceTest {
+
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+
     @Test
     void testCustomersHighestAverageTransactionAmount() throws Exception {
         JSONObject transaction = new JSONObject();
@@ -13,10 +27,13 @@ class FeatureSpaceTest {
         transaction.put("customerId", "Cust123");
         transaction.put("amount", 80);
         JSONObject[] transactions = {transaction};
-        FeatureSpace featureSpace = new FeatureSpace(transactions);
-        assertEquals("Cust123", featureSpace.customersHighestAverageTransactionAmount());
+        FeatureSpace featureSpace = new FeatureSpace("src/main/resources/mockFile.json");
+        featureSpace.customersHighestAverageTransactionAmount();
+        assertEquals("Cust1", outputStreamCaptor.toString().trim());
+    }
 
-
-
+    @AfterEach
+    public void tearDown() {
+        System.setOut(standardOut);
     }
 }
