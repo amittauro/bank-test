36e5c7fedbd3cbbc779f4394419fd06461688d12
Amit Tauro <tauroamit@gmail.com>
2021-03-19 13:18:55 +0000

    adding files to gitignore

--- src/main/java/com/tauro/featurespace/FeatureSpaceReport.java
+++ src/main/java/com/tauro/featurespace/FeatureSpaceReport.java
@@ -3,7 +3,7 @@ package com.tauro.featurespace;
 public class FeatureSpaceReport {
 
     public static void main(String args[]) {
-        FeatureSpace featureSpace = new FeatureSpace("../resources/main/mockFileSix.json");
+        FeatureSpace featureSpace = new FeatureSpace("src/main/resources/exampleInput.json");
         featureSpace.parseFile();
         featureSpace.createReport();
     }
